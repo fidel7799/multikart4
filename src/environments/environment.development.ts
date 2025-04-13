1e77781c3bf4ccc4b1e21962dd994f527f9e20c4
Fidel <fidelbarreat16@gmail.com>
2025-04-13 00:10:00 -0400

    a

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseURL: 'https://multikart4.vercel.app/', // This represents the base URL for running our frontend project.
-  URL: 'https://multikart4.vercel.app/assets/data', // Change only the domain part, keeping "/api" intact
-  storageURL: 'https://multikart4.vercel.app/assets' // Change only the laravel primary domain
+  baseURL: 'http://localhost:4200', // This represents the base URL for running our frontend project.
+  URL: 'http://localhost:4200/assets/data', // Change only the domain part, keeping "/api" intact
+  storageURL: 'http://localhost:4200/assets' // Change only the laravel primary domain
 };
